5e73102a539eb24595a74bf58617a30885701ccb
Joel Kalos <jdk138@case.edu>
2016-11-30 13:36:56 -0500

    first migration made

---
+++ lacwrumosa_app/db/migrate/20161130183601_create_tables.rb
@@ -0,0 +1,8 @@
+class CreateTables < ActiveRecord::Migration[5.0]
+  def change
+    # create_table :tables do |t|
+    # end
+
+    #Input raw create table sql here or rails table creation
+  end
+end
