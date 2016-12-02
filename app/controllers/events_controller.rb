d73d436c570606b1ef40d0f73beea827ae34f75e
Drew Mitchell <hi@callmedrew.com>
2016-12-02 17:26:26 -0500

    Index and show for events.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
+  # def update
+  #   @user = User.find(params[:id])
+  #
+  #   if @user.update_attributes(user_params)
+  #     redirect_to user_path(@user)
+  #   else
+  #     render action: :edit
+  #   end
+  #
+  # end
+
+  private
+
+  # def event_params
+  #   params.require(:user).permit(:id, :Nickname, :GradYear, :Major)
+  # end
+
+end
