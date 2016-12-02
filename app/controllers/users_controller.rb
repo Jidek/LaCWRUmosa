6e469939aa22944b95cf03b660293ddc52a7eacb
Joel Kalos <jdk138@case.edu>
2016-12-02 16:36:38 -0500

    user edit index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,32 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render action: :edit
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :Nickname, :GradYear, :Major)
   end
 
 end
