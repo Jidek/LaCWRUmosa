ad82f6104cd298974dd9cb2fe760914d681b6980
Joel Kalos <jdk138@case.edu>
2016-12-02 17:55:40 -0500

    show page made for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,8 +4,29 @@ class LocationsController < ApplicationController
     @locations = Location.includes(:indoor_location).all
   end
 
+  def show
+    @location = Location.find(params[:id]).includes(:indoor_location)
+  end
+
   def new
     @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.CreatorID = session[:cas_user]
+    @location.CreateTime = DateTime.now
+    if @location.save
+      redirect_to location_url(@location)
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:Description, :Latitude, :Longitude, :IndoorID)
   end
 
 end
