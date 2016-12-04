f8e6877efde083b5012454f827acd873f5518758
Joel Kalos <jdk138@case.edu>
2016-12-04 13:41:15 -0500

    user index is searchable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action CASClient::Frameworks::Rails::Filter
   before_action :authenticate
   protect_from_forgery with: :exception
+  helper_method :params
 
   def authenticate
     if User.where(user_id: session[:cas_user]).blank?
