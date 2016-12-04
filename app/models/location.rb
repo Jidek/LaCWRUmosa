3ec5b735740807efd7a6ed9bfd22247b87bfa841
Drew Mitchell <hi@callmedrew.com>
2016-12-04 16:43:33 -0500

    Geolocation for best locations near me query.

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,9 @@ class Location < ActiveRecord::Base
   belongs_to :indoor_location, optional: true
   has_many :ratings
 
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
+
   def score
     score = 0
     self.ratings.each{|rating| rating.rating? ? score += 1 : score -= 1}
