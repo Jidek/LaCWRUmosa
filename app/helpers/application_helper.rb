cdd4d632298d63c567c733f318cec2e253ffab29
Joel Kalos <jdk138@case.edu>
2016-12-05 00:57:22 -0500

    fully functional feed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,10 @@ module ApplicationHelper
 
   def create_verbose_name_list(event)
     users = event.invites.map{|invite| invite.user}
-    return "#{users.first.name} is " if users.length == 1
-    return "#{users.first.name} and #{users.second.name} are " if users.length == 2
-    return users[0..-2].join(", ") + ", and #{users.last.name} are "
+
+    return "#{link_to users.first.name, user_path(users.first)} is" if users.length == 1
+    return "#{link_to users.first.name, user_path(users.first)} and #{link_to users.second.name, user_path(users.second)} are" if users.length == 2
+    return users[0..-2].map{|user| link_to user.name, user_path(user)}.join(", ") + ", and #{link_to users.last.name, user_path(users.last)} are"
   end
 
 end
