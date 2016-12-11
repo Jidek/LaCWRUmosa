19ae24e550ae7f6d8e3fbbfd307e4c81fc1c3c60
Drew Mitchell <hi@callmedrew.com>
2016-12-10 20:35:20 -0500

    Make geolocation more user-friendly.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,24 @@
 //= require datetimepicker
 //= require_tree .
 
-function populateLocation() {
-  navigator.geolocation.getCurrentPosition(populateCoordinates, notifyError, {maximumAge: 0});
-}
-
-function populateCoordinates(position) {
-  $("#location_latitude").val(position.coords.latitude);
-  $("#location_longitude").val(position.coords.longitude);
-}
-
-function notifyError(error) {
-  if (error.code == 1)
-    alert("If you don&rsquo;t give us your location, we can&rsquo;t find locations near you!");
-  else {
-    alert("There was a problem finding your location.");
-  }
+function populateLocation(event) {
+  var original_value = event.target.value;
+  navigator.geolocation.getCurrentPosition(
+    function(position) { // function on success
+      $("#location_latitude").val(position.coords.latitude);
+      $("#location_longitude").val(position.coords.longitude);
+      event.target.value = original_value;
+      event.target.disabled = false;
+    },
+    function(error) { // function on error
+      event.target.value = original_value;
+      event.target.disabled = false;
+      if (error.code == 1)
+        alert("If you don&rsquo;t give us your location, we can&rsquo;t find locations near you!");
+      else {
+        alert("There was a problem finding your location.");
+      }
+    }, {maximumAge: 0});
+  event.target.value = "Autopopulating location...";
+  event.target.disabled = true;
 }
